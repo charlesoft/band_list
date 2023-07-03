259fee56bbd3fdb69bed26c86eefecbc6ce3d8ea
Charles Furtado <charlees1509@hotmail.com>
2023-07-03 12:05:57 -0300

    Add MemberLive.Index.

--- lib/band_list_web/live/member_live/index.ex
+++ lib/band_list_web/live/member_live/index.ex
@@ -2,10 +2,16 @@ defmodule BandListWeb.MemberLive.Index do
   use BandListWeb, :live_view
 
   alias BandList.Entertainment
+  alias BandList.Entertainment.Member
 
   @impl true
   def mount(%{"band_id" => band_id} = _params, _session, socket) do
-    {:ok, assign(socket, :members, Entertainment.list_members(band_id))}
+    socket =
+      socket
+      |> assign(:members, list_members(band_id))
+      |> assign(:band_id, band_id)
+
+    {:ok, socket}
   end
 
   @impl true
@@ -17,5 +23,34 @@ defmodule BandListWeb.MemberLive.Index do
     socket
     |> assign(:page_title, "Listing Members")
     |> assign(:member, nil)
+  end
+
+  defp apply_action(socket, :new, _params) do
+    socket
+    |> assign(:page_title, "New Member")
+    |> assign(:member, %Member{})
+  end
+
+  defp apply_action(socket, :edit, %{"band_id" => band_id, "id" => id}) do
+    socket
+    |> assign(:page_title, "New Member")
+    |> assign(:member, Entertainment.get_member!(band_id, id))
+  end
+
+  @impl true
+  def handle_event("delete", %{"member" => %{"id" => id, "band_id" => band_id}}, socket) do
+    member = Entertainment.get_member!(band_id, id)
+    {:ok, _member} = Entertainment.delete_member(member)
+
+    {:noreply, assign(socket, :members, list_members(band_id))}
+  end
+
+  @impl true
+  def handle_info({:member_created, member}, socket) do
+    {:noreply, update(socket, :members, fn members -> [member | members] end)}
+  end
+
+  defp list_members(band_id) do
+    Entertainment.list_members(band_id)
   end
 end
